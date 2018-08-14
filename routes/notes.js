daf2df930f7d04b4b64744f8bb51848cc8a5c5b1
Burkey Koontz <bk.koontz@gmail.com>
2018-08-14 13:43:43 -0700

    finished all endpoints

--- routes/notes.js
+++ routes/notes.js
@@ -1,50 +1,105 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const Note = require('../models/note');
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  const searchTerm = req.query.searchTerm;
+  let filter = {};
+  const filterArray = [];
+  
+  if (searchTerm) {
+    const title = { 'title': {$regex: searchTerm, $options: 'i'} };
+    filterArray.push(title);
+    const content = { 'content': {$regex: searchTerm, $options: 'i'} };
+    filterArray.push(content);
+    filter.$or = filterArray;
+  }
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
+  console.log(filterArray);
+  console.log(filter);
 
+  Note
+    .find(filter)
+    .sort({ updatedAt: 'desc' })
+    .then(notes => {
+      res.json(notes);
+    })
+    .catch(error => {
+      next(error);
+    });
 });
+
+
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
-
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+  const id = req.params.id;
+  Note
+    .findById(id)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+ 
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+  if (!('title' in req.body)) {
+    const message = 'Missing title in request body';
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  
+  const newItem = {
+    title: req.body.title,
+    content: req.body.content
+  };
+    
+  Note.create(newItem)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
-
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
+  const idOfItemToUpdate = req.params.id;
+  const updateItem = {};
+  Object.keys(req.body).forEach(key => updateItem[key] = req.body[key]);
+  
+  Note.findByIdAndUpdate(idOfItemToUpdate, updateItem, {new : true})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
+  const idOfItemToRemove = req.params.id;
+  Note
+    .findByIdAndRemove(idOfItemToRemove)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      next(err);
+    });
 
-  console.log('Delete a Note');
-  res.status(204).end();
 });
 
 module.exports = router;
