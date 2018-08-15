ec2bae4a4d62b893c6e007c6cf2ccc9e3ebffb41
Burkey Koontz <bk.koontz@gmail.com>
2018-08-15 11:46:00 -0700

    added before, beforeEach, afterEach, and after to describe block

--- test/notes.js
+++ test/notes.js
@@ -11,4 +11,39 @@ const {TEST_MONGODB_URI} = require('../config');
 
 const Note = require('../models/note');
 
-const seedData = require('../db/seed/notes');
+const seedNotes = require('../db/seed/notes');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Notes API tests'), () => {
+
+  before(function() {
+    // connect to the database 
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connection.db.dropDatabase());
+  });
+
+  beforeEach(function() {
+    // seed the database before each
+    return Note.insertMany(seedNotes);
+  });
+
+  afterEach(function() {
+    // drop the database after each
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function() {
+    // close the connection
+    return mongoose.disconnect();
+  });
+
+
+
+
+
+
+
+};
