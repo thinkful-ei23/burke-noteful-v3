231b191c5085cd0662d0783dd7475ac2c695ac2f
Burkey Koontz <bk.koontz@gmail.com>
2018-08-21 11:51:00 -0700

    added a refresh end point

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,8 @@ const localStrategy = require('../passport/local');
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
 
 function createAuthToken (user) {
   return jwt.sign({ user }, JWT_SECRET, {
@@ -21,6 +23,11 @@ function createAuthToken (user) {
 }
 
 router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
+});
+
+router.post('/refresh', jwtAuth, (req, res) => {
   const authToken = createAuthToken(req.user);
   res.json({ authToken });
 });
