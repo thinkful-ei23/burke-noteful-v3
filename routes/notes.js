a1baace3912b47e31f4c97d854d443509bd77766
Burkey Koontz <bk.koontz@gmail.com>
2018-08-22 09:29:59 -0700

    all endpoints changed to require a userid

--- routes/notes.js
+++ routes/notes.js
@@ -3,8 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const Note = require('../models/note');
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 const passport = require('passport');
+const mongodb = require('mongodb');
 
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
@@ -14,58 +15,25 @@ router.get('/', (req, res, next) => {
   const searchTerm = req.query.searchTerm;
   const folderSearchTerm = req.query.folderId;
   const tagSearchTerm = req.query.tagId;
-  //Add tags to the response
-  // Use .populate() to populate the tags array
-  // Capture the incoming tagId and conditionally add it to the database query filter
-  let filter = {};
+  const userId = req.user.id;
 
-  // filter.folderId = folderSearchTerm; 
-  // 
-  
-
-  const queryCount = Object.keys(req.query).length;
-
+  let filter = {userId};
 
   if (folderSearchTerm) {
-    const folderSearchObj = {folderId : folderSearchTerm};
     filter.folderId = folderSearchTerm;
-    // if (queryCount > 1) {
-    //   filter['$and'].push(folderSearchObj);
-    // } else {
-    //   delete filter.$and;
-    //   filter = folderSearchObj;
-    // }
-
   }
 
   if (tagSearchTerm) {
     filter.tags = tagSearchTerm;
-    // const tagSearchObj = {'tags' : tagSearchTerm};
-    // if (queryCount > 1) {
-    //   filter['$and'].push(tagSearchObj);
-    // } else {
-    //   delete filter.$and;
-    //   filter = tagSearchObj;
-    // }
   }
 
   if (searchTerm) {
-    // we need to set up an explit and
     const searchObject =  {$regex: searchTerm, $options: 'i'};
     const title = {'title': searchObject };
     const content = { 'content': searchObject };
-    const orArray = [title, content];
-    const searchTermObj = { $or : orArray };
-    if (queryCount === 1) {
-      filter.$or = orArray;
-    } else {
-      filter = searchTermObj;
-    }
+    filter.$or = [title, content];
   }
 
-  // if (queryCount === 0) {
-  //   delete filter.$and;
-  // }
   console.log(filter);
 
   Note
@@ -87,7 +55,7 @@ router.get('/:id', (req, res, next) => {
   const id = req.params.id;
   const userId = req.user.id;
 
-  if (!mongodb.Types.ObjectId.isValid(id)) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     const err = new Error('The `id` is not valid');
     err.status = 400;
     return next(err);
@@ -108,6 +76,7 @@ router.get('/:id', (req, res, next) => {
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
   const { title, content, folderId, tags = [] } = req.body;
+  const userId = req.user.id;
 
   /***** Never trust users - validate input *****/
   if (!title) {
@@ -116,7 +85,7 @@ router.post('/', (req, res, next) => {
     return next(err);
   }
 
-  if (folderId && !mongodb.Types.ObjectId.isValid(folderId)) {
+  if (folderId && !mongoose.Types.ObjectId.isValid(folderId)) {
     const err = new Error('The `folderId` is not valid');
     err.status = 400;
     return next(err);
@@ -124,7 +93,7 @@ router.post('/', (req, res, next) => {
 
   if (tags) {
     tags.forEach((tag) => {
-      if (!mongodb.Types.ObjectId.isValid(tag)) {
+      if (!mongoose.Types.ObjectId.isValid(tag)) {
         const err = new Error('The `id` is not valid');
         err.status = 400;
         return next(err);
@@ -132,7 +101,7 @@ router.post('/', (req, res, next) => {
     });
   }
 
-  const newNote = { title, content, folderId, tags };
+  const newNote = { title, content, folderId, tags, userId };
 
   Note.create(newNote)
     .then(result => {
@@ -149,10 +118,17 @@ router.post('/', (req, res, next) => {
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
   const idOfItemToUpdate = req.params.id;
+  const userId = req.user.id;
   const updateItem = {};
   const keyArray = Object.keys(req.body);
 
   keyArray.forEach(key => updateItem[key] = req.body[key]);
+
+  if('userId' in req.body) {
+    const message = 'Cannot change ownership of note';
+    console.error(message);
+    return res.status(400).send(message);
+  }
 
   if (!(keyArray.length)) {
     const message = 'Nothing sent to update';
@@ -170,15 +146,16 @@ router.put('/:id', (req, res, next) => {
 
   if('tags' in req.body) {
     updateItem.tags.forEach(tag => {
-      if (!(mongodb.ObjectID.isValid(tag)) && tag !== '') {
+      if (!(mongoose.ObjectID.isValid(tag)) && tag !== '') {
         const message = 'Not a valid tag id';
         console.error(message);
         return res.status(400).send(message);
       }
     });
   }
-  
-  Note.findByIdAndUpdate(idOfItemToUpdate, updateItem, {new : true})
+
+  // only update an item if that item has the userId and the id to be updated
+  Note.findOneAndUpdate({_id: idOfItemToUpdate, userId}, updateItem, {new : true})
     .then(result => {
       res.json(result);
     })
@@ -191,8 +168,9 @@ router.put('/:id', (req, res, next) => {
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
   const idOfItemToRemove = req.params.id;
+  const userId = req.user.id;
   Note
-    .findByIdAndRemove(idOfItemToRemove)
+    .findOneAndRemove({_id : idOfItemToRemove, userId})
     .then(() => {
       res.status(204).end();
     })
